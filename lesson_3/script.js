53b10520acd762b125284b5b837ddf655ef6accc
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-03-15 18:08:21 +0300

    gg

---
+++ lesson_3/script.js
@@ -0,0 +1,131 @@
+'use strict'
+
+// Masala 1
+
+// let num = prompt('Sonni kiriting')
+
+// if (num > 0) {
+//     let result = +num + 1
+//     alert(result)
+// }else{
+//     alert(num)
+// }
+
+// ==============================================
+
+// Masala 2
+
+// let num = parseInt(prompt('Sonni kiriting'))
+// let result
+
+// if (num > 0){
+//     result = num + 1
+//     alert(result)
+// }else if (num < 0){
+//     result = num - 2
+//     alert(result)
+// }else {
+//     alert('Musbat son kiriting')
+// }
+
+// ==============================================
+
+// Masala 3
+
+// let num = parseInt(prompt('Sonni kiriting'))
+// let result
+
+// if (num < 0){
+//     result = num - 2
+//     alert(result)
+// }else if (num > 0){
+//     result = num + 1
+//     alert(result)
+// }else if (num === 0){
+//     result = num + 10
+//     alert(result)
+// }else{
+//     alert('Son kiriting')
+// }
+
+// ==============================================`
+
+// Masala 4
+
+// let num = -4
+// let num2 = -5
+// let num3 = -6
+
+// if (num > 0 && num2 > 0 && num3 > 0){
+//     console.log("Hamma son musbat")
+// }else if (num > 0 && num2 < 0 && num3 < 0){
+//     console.log("Bitta son musbat")
+// }else if (num < 0 && num2 > 0 && num3 < 0){
+//     console.log("Bitta son musbat")
+// }else if (num < 0 && num2 < 0 && num3 > 0){
+//     console.log("Bitta son musbat")
+// }else if(num > 0 && num2 > 0 && num3 < 0){
+//     console.log("Ikki son musbat")
+// }else if (num < 0 && num2 > 0 && num3 > 0){
+//     console.log("Ikki son musbat")
+// }else if (num > 0 && num2 < 0 && num3 > 0){
+//     console.log("Ikki son musbat")
+// }else if (num < 0 && num2 < 0 && num3 < 0){
+//     console.log("Hamma son manfiy")
+// }else{
+//     console.log("Barcha sonlar manfiy")
+// }
+
+// ==============================================
+
+// Masala 5
+
+// let num = -4
+// let num2 = -5
+// let num3 = 6
+
+// if (num > 0 && num2 > 0 && num3 > 0){
+//     console.log("Hamma son musbat")
+//     console.log("Manfiy yo'q")
+// }else if (num > 0 && num2 < 0 && num3 < 0){
+//     console.log("Bitta son musbat")
+//     console.log("Ikki son manfiy")
+// }else if (num < 0 && num2 > 0 && num3 < 0){
+//     console.log("Bitta son musbat")
+//     console.log("Ikki son manfiy")
+// }else if (num < 0 && num2 < 0 && num3 > 0){
+//     console.log("Bitta son musbat")
+//     console.log("Ikki son manfiy")
+// }else if(num > 0 && num2 > 0 && num3 < 0){
+//     console.log("Ikki son musbat")
+//     console.log("Bitta son manfiy")
+// }else if (num < 0 && num2 > 0 && num3 > 0){
+//     console.log("Ikki son musbat")
+//     console.log("Bitta son manfiy")
+// }else if (num > 0 && num2 < 0 && num3 > 0){
+//     console.log("Ikki son musbat")
+//     console.log("Bitta son manfiy")
+// }else if (num < 0 && num2 < 0 && num3 < 0){
+//     console.log("Musbat yo'q")
+//     console.log("Hamma son manfiy")
+// }else{
+//     console.log("Barcha sonlar manfiy")
+// }
+
+// ==============================================
+
+// Masala 6
+
+// let num = 5,
+//     num2 = 7
+
+// if (num > num2){
+//     console.log(`${num} soni ${num2} sonidan katta`)
+// }else{
+//     console.log(`${num2} soni ${num} sonidan katta`)
+// }
+
+// ==============================================
+
+// Masala 7
+
