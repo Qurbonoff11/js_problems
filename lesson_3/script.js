5f542ff629b2d9a5b155e1b3056cbeaaf6254a0d
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-03-17 21:59:18 +0300

    gg konechka

--- lesson_3/script.js
+++ lesson_3/script.js
@@ -158,3 +158,185 @@
 // ==============================================
 
 // Masala 9
+
+// let num1 = 50,
+//     num2 = 9
+
+// if (num1 < num2){
+//     console.log(`${num1} < ${num2}`)
+// }else{
+//     console.log(`${num1} > ${num2}`)
+// }
+
+// ==============================================
+
+// Masala 10
+
+// let num1 = 5,
+//     num2 = 15
+
+// if (num1 != num2){
+//     let result = num1 + num2
+//     console.log(`${result}`)
+// }else{
+//     let result = num1 - num2
+//     console.log(`${result}`)
+//     console.log(`${num1}, ${num2}`)
+// }
+
+// ==============================================
+
+// Masala 11
+
+// let num1 = 5,
+//     num2 = 5
+
+// if (num1 > num2 && num1 != num2){
+//     console.log(`${num1}`)
+// }else if(num1 < num2 && num1 != num2){
+//     console.log(`${num2}`)
+// }else{
+//     let result = num1 - num2
+//     console.log(`${result}`)
+//     console.log(`${num1}, ${num2}`)
+// }
+
+// ==============================================
+
+// Masala 12
+
+// let num1 = 3,
+//     num2 = 7,
+//     num3 = 5
+
+// if (num1 < num2 && num1 < num3){
+//     console.log(`${num1} eng kiçik son`)
+// }else if (num2 < num1 && num2 < num3){
+//     console.log(`${num2} eng kiçik son`)
+// }else if(num3 < num1 && num3 < num2){
+//     console.log(`${num3} eng kiçik son`)
+// }else{
+//     console.log('Sonlar teng')
+// }
+
+// ==============================================
+
+// Masala 13
+
+// let num1 = 3,
+//     num2 = 7,
+//     num3 = 9
+
+// if (num1 > num2 && num1 < num3){
+//     console.log(`${num1} o'rtacha son`)
+// }else if (num2 > num1 && num2 < num3){
+//     console.log(`${num2} o'rtacha son`)
+// }else if(num3 > num1 && num3 < num2){
+//     console.log(`${num3} o'rtacha son`)
+// }else{
+//     console.log('Sonlar teng')
+// }
+
+// ==============================================
+
+// Masala 14
+
+// let num1 = 3,
+//     num2 = 7,
+//     num3 = 9,
+//     largeNum,
+//     smallNum
+
+// if (num1 > num2 && num1 > num3){
+//     largeNum = num1
+//     console.log(`Eng katta son: ${largeNum}`)
+// }else if (num2 > num1 && num2 > num3){
+//     largeNum = num2
+//     console.log(`Eng katta son: ${largeNum}`)
+// }else if (num3 > num1 && num3 > num2){
+//     largeNum = num3
+//     console.log(`Eng katta son: ${largeNum}`)
+// }
+
+// if (num1 < num2 && num1 < num3){
+//     smallNum = num1
+//     console.log(`Eng kichik son: ${smallNum}`)
+// }else if (num2 < num1 && num2 < num3){
+//     smallNum = num2
+//     console.log(`Eng kichik son: ${smallNum}`)
+// }else if (num3 < num1 && num3 < num2){
+//     smallNum = num3
+//     console.log(`Eng kichik son: ${smallNum}`)
+// }else{
+//     console.log('Sonlar teng')
+// }
+
+// ==============================================
+
+// Masala 15
+
+// let num1 = 3,
+//     num2 = 7,
+//     num3 = 9,
+//     result1,
+//     result2
+
+// if (num1 + num2 > num1 + num3 && num1 + num2 > num2 + num3) {
+//     result1 = num1;
+//     result2 = num2;
+// } else if (num1 + num3 > num1 + num2 && num1 + num3 > num2 + num3) {
+//     result1 = num1;
+//     result2 = num3;
+// } else if (num2 + num3 > num1 + num2 && num2 + num3 > num1 + num3) {
+//     result1 = num2;
+//     result2 = num3;
+// }else{
+//     console.log('Sonlar teng')
+// }
+
+// console.log(`Eng katta sonlar: ${result1 + result2}`);
+
+// ==============================================
+
+// Masala 16
+
+// let num1 = 7,
+//     num2 = 9,
+//     num3 = 11
+
+// if (num1 < num2 && num2 < num3){
+//     num1 = num1 * 2
+//     num2 = num2 * 2
+//     num3 = num3 * 2
+//     console.log(`${num1}, ${num2}, ${num3}`)
+// }else{
+//     num1 = num1 / 2
+//     num2 = num2 / 2
+//     num3 = num3 / 2
+//     console.log(`${num1}, ${num2}, ${num3}`)
+// }
+
+// ==============================================
+
+// Masala 17
+
+// let num1 = 71,
+//     num2 = 19,
+//     num3 = 11
+
+// if (num1 < num2 && num2 < num3){
+//     num1 = num1 * 2
+//     num2 = num2 * 2
+//     num3 = num3 * 2
+//     console.log(`${num1}, ${num2}, ${num3}`)
+// }else if (num1 > num2 && num2 > num3){
+//     num1 = num1 * 2
+//     num2 = num2 * 2
+//     num3 = num3 * 2
+//     console.log(`${num1}, ${num2}, ${num3}`)
+// }else{
+//     num1 = num1 / 2
+//     num2 = num2 / 2
+//     num3 = num3 / 2
+//     console.log(`${num1}, ${num2}, ${num3}`)
+// }
