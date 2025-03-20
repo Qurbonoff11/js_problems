ac67ebe766e4a65adf06683a12ceb2ed521044a2
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-03-20 16:08:36 +0300

    gg konechka

--- lesson_4/script.js
+++ lesson_4/script.js
@@ -1,23 +1,203 @@
 'use strict';
 
-// Masala 1
+// =================== 1-Masala ===================
 
-let price = 165,
+// let price = 120,
+//     result,
+//     items = "go'sht",
+//     sale;
+
+// switch (items) {
+//     case "meva":
+//         sale = 10;
+//         result = price - (price * sale) / 100;
+//         console.log(`Jami: $${result}`);
+//         break;
+//     case "sut maxsuloti":
+//         sale = 5;
+//         result = price - (price * sale) / 100;
+//         console.log(`Jami: $${result}`);
+//         break;
+//     case "go'sht":
+//         sale = 15;
+//         result = price - (price * sale) / 100;
+//         console.log(`Jami: $${result}`);
+//         break;
+//     case "non":
+//         console.log(`Jami: $${result}`);
+//         break;
+//     default:
+//         console.log('Xatolik yuz berdi');
+//         break;
+// }
+
+
+
+// =================== 2-Masala ===================
+
+// let status = window.prompt("Statusingizni kiriting(admin, guest, user)")
+
+// switch (status) {
+//     case "admin":
+//         window.location.href = "admin.html";
+//         break;
+//     case "guest":
+//         window.location.href = "guest.html";
+//         break;
+//     case "user":
+//         window.location.href = "user.html";
+//         break
+//     default:
+//         break;
+// }
+
+// =================== 3-Masala ===================
+
+// let weekDay = parseInt(window.prompt("Hafta kunini kiriting (1-7)"))
+// let weekDay = 5;
+
+// switch (weekDay) {
+//     case 1:
+//         console.log('Dushanba');
+//         break;
+//     case 2:
+//         console.log('Seshanba');
+//         break;
+//     case 3:
+//         console.log('Chorshanba');
+//         break;
+//     case 4:
+//         console.log('Payshanba');
+//         break;
+//     case 5:
+//         console.log('Juma');
+//         break;
+//     case 6:
+//         console.log('Shanba');
+//         break;
+//     case 7:
+//         console.log('Yakshanba');
+//         break;
+//     default:
+//         console.log('Notogri kun kiritildi');
+//         break;
+// }
+
+// =================== 4-Masala ===================
+
+// let price = 165,
+//     result;
+
+// switch (true){
+//     case price < 50:
+//         result = price + 10;
+//         console.log(`Jami narxi: ${result} $`);
+//         break;
+//     case price > 50 && price < 100:
+//         result = price + 5;
+//         console.log(`Jami narxi: ${result} $`);
+//         break;
+//     case price > 100:
+//         result = price;
+//         console.log(`Jami narxi: ${result} $`);
+//         break;
+//     default:
+//         console.log('Xatolik yuzberdi');
+// }
+
+// =================== 5-Masala ===================
+
+// let time = "6:00"
+
+// switch (time) {
+//     case "6:00":
+//     case "7:00":
+//     case "8:00":
+//     case "9:00":
+//     case "10:00":
+//     case "11:00":
+//         console.log('Xayrli tong');
+//         break;
+//     case "12:00":
+//     case "13:00":
+//     case "14:00":
+//     case "15:00":
+//     case "16:00":
+//     case "17:00":
+//     case "18:00":
+//         console.log('Xayrli kun');
+//         break;
+//     case "19:00":
+//     case "20:00":
+//     case "21:00":
+//     case "22:00":
+//         console.log('Xayrli kech');
+//         break
+//     case "23:00":
+//     case "00:00":
+//     case "01:00":
+//     case "02:00":
+//     case "03:00":
+//     case "04:00":
+//     case "05:00":
+//         console.log('Xayrli tun');
+//         break;
+//     default:
+//         break;
+// }
+
+// =================== 6-Masala ===================
+
+// let payment = "click";
+
+// switch (payment) {
+//     case "naqd":
+//         console.log(`To'lov ${payment} ko'rinishida amalga oshirildi`);
+//         break;
+//     case "karta":
+//         console.log(`To'lov ${payment} orqali amalga oshirildi`);
+//         break;
+//     case "click":
+//         console.log(`To'lov ${payment} orqali amalga oshirildi`);
+//         break;
+//     default:
+//         console.log("Xatolik yuz berdi");
+//         break;
+// }
+
+// =================== 7-Masala ===================
+
+/*
+let valute = prompt("Valyutani tanlang (USD, EUR, RUB)").toUpperCase(),
+    sum = parseInt(prompt("Qancha pul o'zgartirmoqchisiz?")),
     result;
 
-switch (true){
-    case price < 50:
-        result = price + 10;
-        console.log(`Jami narxi: ${result} $`);
+    // let valute = "USD",
+    //     sum = 100,
+    //     result;
+
+const USD = 12929.55
+const EUR = 14101
+const RUB = 152.36
+
+switch (valute) {
+    case "USD":
+        result = sum * USD;
+        console.log(`$${sum} = ${result} so'm`);
+        alert(`$${sum} = ${result} so'm`);
         break;
-    case price > 50 && price < 100:
-        result = price + 5;
-        console.log(`Jami narxi: ${result} $`);
+    case "EUR":
+        result = sum * EUR;
+        console.log(`€${sum} = ${result} so'm`);
+        alert(`€${sum} = ${result} so'm`);
         break;
-    case price > 100:
-        result = price;
-        console.log(`Jami narxi: ${result} $`);
+    case "RUB":
+        result = sum * RUB;
+        console.log(`₽${sum} = ${result} so'm`);
+        alert(`₽${sum} = ${result} so'm`);
         break;
     default:
-        console.log('Xatolik yuzberdi');
+        console.log('Xatolik yuz berdi');
+        break;
 }
+*/
