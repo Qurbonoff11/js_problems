123e91655b84491f413f43692a69d0be7c7c5588
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-03-31 23:58:19 +0300

    add

---
+++ script.js
@@ -0,0 +1,31 @@
+// let text = "salom"
+
+// for (let i = 0; i < 1000; i++){
+//     console.log(text);
+// }
+
+// ==================
+
+// console.log("a" > "b" ? "a katta" : "b katta");
+
+// ===================
+
+// function trickyScope() {
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+
+//     if (true) {
+//         var a = 40;
+//         let b = 50;
+//         const c = 60;
+//         console.log(a, b, c);
+//     }
+
+//     console.log(a, b, c);
+// }
+
+// trickyScope();
+
+// ===================
+
